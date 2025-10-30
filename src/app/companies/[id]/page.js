fda71e494cbdc482b27921fbba8e3039f4b59282
SAMANTHA PARKES <slmillward@outlook.com>
2025-10-30 14:31:39 +0000

    add review and submit to db

--- src/app/companies/[id]/page.js
+++ src/app/companies/[id]/page.js
@@ -1,8 +1,11 @@
 import CompanyDetails from "@/app/components/CompanyDetails";
 import { db } from "@/app/utils/utilities";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function Page({ params }) {
   const { id } = await params;
+  const { userId } = await auth();
 
   const company = (
     await db.query(`SELECT * FROM companies WHERE id = $1`, [id])
@@ -14,16 +17,41 @@ export default async function Page({ params }) {
       [id]
     )
   ).rows;
+
+  async function handleSubmit(formData) {
+    "use server";
+
+    const content = formData.get("content");
+
+    const res = await db.query(
+      `SELECT id FROM user_accounts WHERE clerk_id = $1`,
+      [userId]
+    );
+    const currentUserId = res.rows[0].id;
+
+    await db.query(
+      `INSERT INTO company_reviews (user_id, company_id, content) VALUES ($1, $2, $3)`,
+      [currentUserId, id, content]
+    );
+
+    redirect(`/companies/${id}`);
+  }
+
   return (
     <div>
       <CompanyDetails company={company} />
       <h2>What is it like to work at {company.name}?</h2>
       {reviews.map((review) => (
-        <li key={review.id}>
+        <div key={review.id}>
           <p>{review.username}</p>
           <p>{review.content}</p>
-        </li>
+        </div>
       ))}
+      <form action={handleSubmit}>
+        <h2>Tell others about your experience with {company.name}</h2>
+        <input name="content" placeholder="e.g. heres my review" required />
+        <button type="submit">Submit review</button>
+      </form>
     </div>
   );
 }
