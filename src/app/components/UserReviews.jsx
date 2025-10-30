ba1f4f839445eb7bd7d2a8b2965675904597b5e3
SAMANTHA PARKES <slmillward@outlook.com>
2025-10-30 16:38:22 +0000

    delete review from user profile page

--- src/app/components/UserReviews.jsx
+++ src/app/components/UserReviews.jsx
@@ -1,9 +1,14 @@
 import { db } from "../utils/utilities";
 import Link from "next/link";
+import DeleteButton from "./DeleteButton";
+import { redirect } from "next/navigation";
 
 export default async function UserReviews({ userId }) {
   const reviews = (
-    await db.query(`SELECT * FROM company_reviews WHERE user_id = $1`, [userId])
+    await db.query(
+      `SELECT company_reviews.id AS review_id, company_reviews.content, companies.name, companies.id AS company_id FROM company_reviews JOIN companies ON companies.id = company_reviews.company_id WHERE company_reviews.user_id = $1`,
+      [userId]
+    )
   ).rows;
 
   if (reviews.length === 0) {
@@ -15,13 +20,24 @@ export default async function UserReviews({ userId }) {
     );
   }
 
+  async function handleDelete(reviewId) {
+    "use server";
+    await db.query(`DELETE FROM company_reviews WHERE id = $1`, [reviewId]);
+
+    redirect(`/users`);
+  }
+
   return (
     <div>
       <h2>Reviews youve submitted</h2>
       {reviews.map((review) => (
-        <div key={review.id}>
-          <p>Anonymous {review.id}</p>
+        <div key={review.review_id}>
+          <p>{review.name}</p>
           <p>{review.content}</p>
+          <DeleteButton
+            reviewId={review.review_id}
+            handleDelete={handleDelete}
+          />
         </div>
       ))}
     </div>
