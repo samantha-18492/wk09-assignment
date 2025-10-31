7eb2bcf0594f8b0c04f9b561ddfd2f85cbb3590d
SAMANTHA PARKES <slmillward@outlook.com>
2025-10-31 15:56:09 +0000

    not-found

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,7 +5,10 @@ export default function notFound() {
   return (
     <div className="mt-20 text-center">
       <h2>Oops! That page doesn&apos;t exist.</h2>
-      <p>Check back soon or return home.</p>
+      <p>
+        Sometimes even the best career journeys hit a dead end. Return home to
+        get back on track.
+      </p>
       <Link
         href="/"
         className={`${zalandoSemiExpanded.className} bg-ww-deep-blue text-white rounded-full shadow-md shadow-gray-400 inline-block py-4 px-6 max-w-60 mt-10 hover:bg-ww-orange hover:text-black hover:font-bold`}
